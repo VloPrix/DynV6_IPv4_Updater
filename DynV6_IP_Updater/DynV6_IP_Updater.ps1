98d617cbe144119ac2e640f52dd80a09855f4542
VloPrix <98618059+VloPrix@users.noreply.github.com>
2022-01-29 02:54:26 +0100

    Update for DynV6_IP_Updater
    Added a check if a Token is given as a parameter. Else it will ask for a token. Added Comments for better understanding.

--- DynV6_IP_Updater/DynV6_IP_Updater.ps1
+++ DynV6_IP_Updater/DynV6_IP_Updater.ps1
@@ -1,15 +1,34 @@
-Get-Content .\hostnames.txt | foreach-object {CreateRequest $_}
+param (
+    #Parameter for token.
+    $token
+)
 
 
-function CreateRequest {
+#Checks if the value of the token variable is zero. If it is zero it will ask for the token.
+if ($token -eq $null) {
+    Write-Host "No Token given as Parameter. Please paste your token below."
+    $token = Read-Host "Token: " 
+}
+
+
+#Gets the content of the hostnames.txt file. For each line it will call the createRequest function with the value of the line.
+Get-Content .\hostnames.txt | foreach-object {createRequest $_}
+
+
+function createRequest {
     param (
+        #Parameter for the Hostname that was read from the hostnames.txt file
         $hostname
     )
+
+    #Saves the Date in the date variable
     $date = Get-date
 
-    
 
-    Invoke-Webrequest -URI https://dynv6.com/api/update?ipv4=auto'&'token=<token>'&'hostname=$hostname | Add-content -Path .\output.log
-
+    #Adding the lines with the Date and the Hostname for a better overview
     Add-Content -Path .\output.log -Value "--------------------------------", $date, "Hostname: $hostname"
+
+
+    #Sends a Webrequest to dynv6.com with the given Hostname and Token and writes the output to the output.log file
+    Invoke-Webrequest -URI https://dynv6.com/api/update?ipv4=auto'&'token=$token'&'hostname=$hostname | Add-content -Path .\output.log    
 }
