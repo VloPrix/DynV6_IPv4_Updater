5b66c01bf16ead8410385b06b83ab4c9bef675b8
VloPrix <98618059+VloPrix@users.noreply.github.com>
2022-01-29 19:30:35 +0100

    Added a Logrotate function

--- DynV6_IPv4_Updater/DynV6_IPv4_Updater.ps1
+++ DynV6_IPv4_Updater/DynV6_IPv4_Updater.ps1
@@ -3,6 +3,26 @@ param (
     $token
 )
 
+
+#Checks if the file output.log is existing
+$logresult = Test-Path ".\output.log"
+
+#if the size of output.log exceeds 20 KB, output.log gets renamed to output.log.2. If a output.log.2 is already present, it will be deleted
+if ($logresult -eq "True") {
+    $outputsize = (Get-ChildItem -Path .\output.log).Length/1KB
+    if ($outputsize -ge 20){
+    
+        $result = Test-Path ".\output.log.2"
+        if ($result -eq "True") {
+            Remove-Item ".\output.log.2"
+        }
+    Rename-Item -Path .\output.log -NewName "output.log.2"
+    }
+}
+
+
+
+
 function createRequest {
     param (
         #Parameter for the Hostname that was read from the hostnames.txt file
