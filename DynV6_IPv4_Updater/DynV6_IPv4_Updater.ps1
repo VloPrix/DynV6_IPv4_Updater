a78ca459421c14855c0659594290c431c7e4aa78
VloPrix <98618059+VloPrix@users.noreply.github.com>
2022-01-29 15:24:30 +0100

    Fixed Function not recognized

--- DynV6_IPv4_Updater/DynV6_IPv4_Updater.ps1
+++ DynV6_IPv4_Updater/DynV6_IPv4_Updater.ps1
@@ -3,18 +3,6 @@ param (
     $token
 )
 
-
-#Checks if the value of the token variable is zero. If it is zero it will ask for the token.
-if ($token -eq $null) {
-    Write-Host "No Token given as Parameter. Please paste your token below."
-    $token = Read-Host "Token: " 
-}
-
-
-#Gets the content of the hostnames.txt file. For each line it will call the createRequest function with the value of the line.
-Get-Content .\hostnames.txt | foreach-object {createRequest $_}
-
-
 function createRequest {
     param (
         #Parameter for the Hostname that was read from the hostnames.txt file
@@ -32,3 +20,14 @@ function createRequest {
     #Sends a Webrequest to dynv6.com with the given Hostname and Token and writes the output to the output.log file
     Invoke-Webrequest -URI https://dynv6.com/api/update?ipv4=auto'&'token=$token'&'hostname=$hostname | Add-content -Path .\output.log    
 }
+
+
+#Checks if the value of the token variable is zero. If it is zero it will ask for the token.
+if ($token -eq $null) {
+    Write-Host "No Token given as Parameter. Please paste your token below."
+    $token = Read-Host "Token: " 
+}
+
+
+#Gets the content of the hostnames.txt file. For each line it will call the createRequest function with the value of the line.
+Get-Content .\hostnames.txt | foreach-object {createRequest $_}
